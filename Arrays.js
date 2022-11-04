8b9b9cd0ac56a7582d7033e2443d3d5f7ec57466
Matthew Joseph <matthew.j.joe@gmail.com>
2022-11-04 07:41:15 -0400

    added some new array operations to array.js

--- Arrays.js
+++ Arrays.js
@@ -4,18 +4,36 @@
 // data in memory. 
 
 
-// There are three main operations one could perform on an array. Get, Set, and Init.
+// There are 6 core operations one could perform on an array. Get, Set, Init, Traverse/Looping, Copy, and Insertion.
 
 // Init
-// O(n)[space] x O(n)[time] - this is because the computer needs to find space in memory to fit the array values in sequential order. The larger the array, the more space space needs to be found. 
+// O(n)[space]  O(n)[time] - this is because the computer needs to find space in memory to fit the array values in sequential order. The larger the array, the more space space needs to be found. 
 const computers = ["MacBook pro", "Dell xps", "Chromebook"];
 
+
 // Get
-// O(1)[space] x O(1)[time] - getting an elements constant time because we know exactly where the element is +  we are not using any extra space.
+// O(1)[space]  O(1)[time] - getting an elements constant time because we know exactly where the element is +  we are not using any extra space.
 computers[2];
- 
-//Set
-// O(1)[space] O(1)[time] - setting an element would also be constant time and space because we know the Length of the arr, so 
+
+
+// Set
+//  O(1)[space]  O(1)[time] - setting/overwriting an element would also be constant time and space because we are not changing the amount of space needed, and we already know where the element is
 computers[1] = "Not A MacBook";
+
+
+// Traverse
+// O(1)[space]  O(n)[time] - if you are ONLY traversing you don't need to store anything new, meaning constant space. Alternatively, when looping through the array, the time needed to do so depends, or scales linearly, to the length of the array.
+for(let interval = 0; interval < computers.length; interval++){
+    console.log(`Computer ${interval}: ` + computers[interval]);
+}
+
+
+// Copy
+// O(n)[space]  O(n)[time] - copying requires you to find space for the new array + the time needed will scale linearly with the length of the array 
+const newArray = computers.slice();
+
+
+// Insertion
+// 
 
 console.log(computers);
