f6ae998cac9c5324f5edc167a1db042ab9e2c61a
Matthew Joseph <matthew.j.joe@gmail.com>
2022-11-11 22:12:18 -0500

    added new insertion section to array.js

--- Arrays.js
+++ Arrays.js
@@ -9,6 +9,7 @@
 // Init
 // O(n)[space]  O(n)[time] - this is because the computer needs to find space in memory to fit the array values in sequential order. The larger the array, the more space space needs to be found. 
 const computers = ["MacBook pro", "Dell xps", "Chromebook"];
+console.table({computers});
 
 
 // Get
@@ -24,16 +25,21 @@ computers[1] = "Not A MacBook";
 // Traverse
 // O(1)[space]  O(n)[time] - if you are ONLY traversing you don't need to store anything new, meaning constant space. Alternatively, when looping through the array, the time needed to do so depends, or scales linearly, to the length of the array.
 for(let interval = 0; interval < computers.length; interval++){
-    console.log(`Computer ${interval}: ` + computers[interval]);
+    console.table(`Computer ${interval}: ` + computers[interval]);
 }
 
 
 // Copy
 // O(n)[space]  O(n)[time] - copying requires you to find space for the new array + the time needed will scale linearly with the length of the array 
-const newArray = computers.slice();
+const newArray = computers.slice(0, 2);
+console.table({newArray}); 
 
 
 // Insertion
-// 
+// Inserting into an array will be very taxing because you must traverse the whole array and make room for the new value. This will take O(n) time and O(1) space because the OS will free up the initial place the array was held
+computers.push("Samsung");
+console.table({computers})
 
-console.log(computers);
+const t = [-7, -5, -3, -1, 0, 1, 3, 5, 7];
+t.sort((a,b) => {return a-b});
+console.log(t);
