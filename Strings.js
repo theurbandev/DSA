ce5a6d7c63b03a870461ffda409d5373e84737af
Matthew Joseph <matthew.j.joe@gmail.com>
2022-11-17 11:15:10 -0500

    added changes to strings.js

--- Strings.js
+++ Strings.js
@@ -1,31 +1,32 @@
 // Examples of string problems
 
 
+// Caesar Cipher Encryption
 /* Given a non-empty string of lowercase letters and a non-negative integer representing a key, write a function
 that returns a new string obtained by shifting every letter in the given string input, but k positions in the alphabet, where k is the key */
 
+// [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+
 function caesarCipherEncryptor(string, key) {
-    // Example below: 
-    // "timmy" <- shift by k positions..
-    // k = 1 -> "timmy" becomes "ujnnz"
-    // O(n) Space Time Complexity 
-
-    return shift(string, key).join("");
-}
+    // Write your code here.
+      let output = [];
+      let newKey = key % 26;
+      
+      for(const letters of string){
+          let newLetters = getNewLetter(letters, newKey);
+          output.push(newLetters);
+      }
+      console.log(output.join(""));
+      return output.join("");
+  }
   
-function shift(string, key){
-    let shiftArr = [];
-    let newLetterCode = "";
+  function getNewLetter(letters, key){
+      //turn the letters into the appropriate nums (according to the key)
+      //return the new nums as letters
+      newLetterCode = letters.charCodeAt() + key;
+      return newLetterCode <= 122 ? String.fromCharCode(newLetterCode) : String.fromCharCode(96 + (newLetterCode % 122));
+  }
+  
 
-    for (let index = 0; index < string.length; index++) {
-        // console.log(string.charAt(index + key));
-        newLetterCode = string.charCodeAt(string[index] + key);      
-        shiftArr.push(string[index].charAt(index)); 
-    }
-
-    console.log(shiftArr);
-    return shiftArr;
-}
-
-  caesarCipherEncryptor("abc", 2);
+  caesarCipherEncryptor("zback", 2);
    
